bebefce2f40270acaea2098e4080d1a70264879a
sonnema <nemasonia@gmail.com>
2018-09-18 23:01:46 +0100

    multiple changes

--- OnlineShopping/WebContent/WEB-INF/js/mksk.js
+++ OnlineShopping/WebContent/WEB-INF/js/mksk.js
@@ -1,3 +1,4 @@
+
 var slideIndex = 0;
 showSlides();
 
@@ -18,4 +19,64 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 5000); // Change image every 2 seconds
+};
+
+/* Opening and Closing of Modal Boxes */
+
+function closestEl(el, selector) {
+    var doc = el.document || el.ownerDocument;
+    var matches = doc.querySelectorAll(selector);
+    var i;
+    while (el) {
+        i = matches.length - 1;
+        while (i >= 0) {
+            if (matches.item(i) === el) {
+                return el;
+            }
+            i -= 1;
+        }
+        el = el.parentElement;
+    }
+    return el;
 }
+var modalLnks = document.querySelectorAll(".modalLink");
+modalLnks.forEach(function addBtnClickEvent(lnk) {
+    lnk.onclick = function showModal() {
+        var modal = lnk.getAttribute("data-modal");
+        document.getElementById(modal).style.display = "block";
+    };
+});
+
+var closeBtns = document.querySelectorAll(".close");
+closeBtns.forEach(function addCloseClickEvent(lnk) {
+    lnk.onclick = function closeModal() {
+        var modal = closestEl(lnk, ".modal");
+        modal.style.display = "none";
+    };
+});
+
+window.onclick = function closeOnClick(event) {
+    if (event.target.className === "modal") {
+        event.target.style.display = "none";
+    }
+};
+
+/*
+function openLink(evt, linkName) {
+	console.log("evt : " + evt + " linkName : " + linkName);
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    console.log(tabcontent);
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        console.log(tabcontent[i]);
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        console.log(tablinks[i].className);
+    }
+    document.getElementById(linkName).style.display = "block";
+    evt.currentTarget.className += " active";
+};
+*/
